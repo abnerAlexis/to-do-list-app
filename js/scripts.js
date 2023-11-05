f0d6cec07252ec3558a429877655cd1c65069520
Alexis Abner <alexisabner@alexiss-air.lan>
2023-11-04 22:32:04 -0600

    Create crossOut function, add event listener

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,17 @@ let inputValue = $('#input').val();
 // let text = document.createTextNode(inputValue); 
 //inputValue/text is added to the list
 li.append(inputValue);
+//If inputValue is not entered, alert will be given, else inputValue will be added to the list.
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    li.append(inputValue);
+  }
+
+  //Crossing out a list item:
+  function crossOut() {
+    li.addClass('strike');
+}
+
+//Event listener waits for double click to crossOut a list item
+$("li").on("dblclick", crossOut);
