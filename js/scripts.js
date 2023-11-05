c903a645620d6ea0a57108858256191f395c8cee
Alexis Abner <alexisabner@alexiss-air.lan>
2023-11-04 22:02:28 -0600

    use style from the repl

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,6 @@
 let li = $('<li></li>');
 //adding the list element to the ordered list
 list.append(li);
+//Get input value and store it to inputValue
+let inputValue = $('#input').val();
+let text = document.createTextNode(inputValue);
