e31995ca3c10ec4443c20da10af1b91c2120ddd9
Alexis Abner <alexisabner@alexiss-air.lan>
2023-11-06 12:15:24 -0700

    Play around the code to understand functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,39 @@
-//Assigning a list element to a variable.
-let li = $('<li></li>');
-//adding the list element to the ordered list
-list.append(li);
-//Get input value and store it to inputValue
-let inputValue = $('#input').val();
-// let text = document.createTextNode(inputValue); 
-//inputValue/text is added to the list
-li.append(inputValue);
-//If inputValue is not entered, alert will be given, else inputValue will be added to the list.
+function newItem() {
+
+  // Add new list item
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  //Add if/else condition to check if user enters text before
+  //clicking add button
   if (inputValue === '') {
-    alert("You must write something!");
+    alert('You must write something before clicking Add button.')
   } else {
-    li.append(inputValue);
+    let list = $('#list');
+    list.append(li);
   }
 
-  //Crossing out a list item:
+  // Add strike function to croos out a list item
   function crossOut() {
-    li.addClass('strike');
-}
+    li.toggleClass('strike');
+  }
+  li.on('dblclick', crossOut);
 
-//Event listener waits for double click to crossOut a list item
-$("li").on("dblclick", crossOut);
+  // Create delete button "X"
+
+  let crossOutButton = $('<button></button>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on('click', deleteListItem);
+
+  //Add function to delete a list item m- class delete
+  function deleteListItem() {
+    li.addClass('delete');
+  }
+
+  // Reorder items
+  $('#list').sortable();
+
+}
